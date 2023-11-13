c8330ca475d6c9558ce8c7e059b4dec2fa1e9891
aleksejs <>
2023-11-13 15:39:14 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+export const config = {
+    STG: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'c4b0d9b007bb299ae3f49a83373c0fc87f8ae2e9f1bf6d1fe25b89dd23f48621',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    },
+    PROD: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'f0e3ab885dc644e66a1ef484744d062e03b44b06f41acd67d19c808bfdfa0f3d',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    }
+}
+
+global.executionVariables = {}
